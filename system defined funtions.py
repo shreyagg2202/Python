f16be447ddeb5270babd73f4aeaf505814c7d420
Shrey Aggarwal <77096508+shreyagg2202@users.noreply.github.com>
2021-01-13 00:07:48 +0530

    Delete system defined funtions.py

--- system defined funtions.py
+++
@@ -1,8 +0,0 @@
-import datetime
-
-print("Current date and time : ",datetime.datetime.now()
-print("Current Year : ",datetime.date.today().strftime("%Y"))
-print("Month of Year : ",datetime.datetime.now())
-print("Current date and time : ",datetime.datetime.now())
-print("Current date and time : ",datetime.datetime.now())
-print("Current date and time : ",datetime.datetime.now())
