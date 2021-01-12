d06049a10f08338a436f0ac3d82ff594a5e01d01
Shrey Aggarwal <77096508+shreyagg2202@users.noreply.github.com>
2021-01-12 23:49:10 +0530

    Delete positive or negative.py

--- positive or negative.py
+++
@@ -1,7 +0,0 @@
-num = int(input("Enter the number to be checked:"))
-if(num>0):
-    print("The number is positive")
-elif(num<0):
-    print("The number is negative")
-else:
-    print("The number is 0")
